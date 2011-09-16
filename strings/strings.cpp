28ebb4f871f56081383023d8e6c121a3ba9b6a76
Vahan Aghajanyan <vahancho@gmail.com>
2011-09-16 22:23:34 +0200

    Update the application version string.

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -72,6 +72,6 @@ namespace str
 
     // Version related strings
     const char *sVersionUrl = "http://gherald.sourceforge.net/version";
-    const char *sVersion = "0.2.226 (beta)";
+    const char *sVersion = "0.3";
 
 } // namespace str
