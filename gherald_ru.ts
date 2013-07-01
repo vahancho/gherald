9dcc8c76178acceeee2389d28e0e2e405afbf15e
Vahan Aghajanyan <vahancho@gmail.com>
2013-07-01 22:35:32 +0200

    Move translation file to the project root directory.

--- gherald_ru.ts
+++ gherald_ru.ts
@@ -4,238 +4,266 @@
 <context>
     <name>GHerald</name>
     <message>
-        <location filename="strings/guiStrings.cpp" line="30"/>
-        <source>&lt;table cellspacing=2 cellpadding=2&gt;&lt;tr valign=&quot;top&quot;&gt;&lt;td&gt;&lt;img src=&quot;:icons/notifier&quot; width=&quot;32&quot; height=&quot;32&quot;&gt;&lt;br&gt;&lt;/a&gt;&lt;/td&gt;&lt;td&gt;&lt;span foreground=&quot;#000000&quot;&gt;&lt;small&gt;%1 unread messages...&lt;/small&gt;&lt;/span&gt;&lt;BR&gt;&lt;BR&gt;%2&lt;BR&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;</source>
+        <location filename="gherald/strings/guiStrings.cpp" line="30"/>
+        <source>&lt;table cellspacing=2 cellpadding=2&gt;&lt;tr valign=&quot;top&quot;&gt;&lt;td&gt;&lt;img src=&quot;:icons/notifier&quot; width=&quot;32&quot; height=&quot;32&quot;&gt;&lt;br&gt;&lt;/a&gt;&lt;/td&gt;&lt;td&gt;&lt;span foreground=&quot;#000000&quot;&gt;&lt;small&gt;%1 unread messages...&lt;/small&gt;&lt;/span&gt;&lt;BR&gt;&lt;BR&gt;%2&lt;BR&gt;&lt;BR&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;</source>
+        <oldsource>&lt;table cellspacing=2 cellpadding=2&gt;&lt;tr valign=&quot;top&quot;&gt;&lt;td&gt;&lt;img src=&quot;:icons/notifier&quot; width=&quot;32&quot; height=&quot;32&quot;&gt;&lt;br&gt;&lt;/a&gt;&lt;/td&gt;&lt;td&gt;&lt;span foreground=&quot;#000000&quot;&gt;&lt;small&gt;%1 unread messages...&lt;/small&gt;&lt;/span&gt;&lt;BR&gt;&lt;BR&gt;%2&lt;BR&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;</oldsource>
         <translation>&lt;table cellspacing=2 cellpadding=2&gt;&lt;tr valign=&quot;top&quot;&gt;&lt;td&gt;&lt;img src=&quot;:icons/notifier&quot; width=&quot;32&quot; height=&quot;32&quot;&gt;&lt;br&gt;&lt;/a&gt;&lt;/td&gt;&lt;td&gt;&lt;span foreground=&quot;#000000&quot;&gt;&lt;small&gt;%1 непрочитанных сообщений...&lt;/small&gt;&lt;/span&gt;&lt;BR&gt;&lt;BR&gt;%2&lt;BR&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="45"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="46"/>
         <source>Gmail Herald %1&lt;BR&gt;Copyright © 2010-2013 &lt;a href=&quot;mailto:vahancho@gmail.com&quot;&gt;Vahan Aghajanyan&lt;/a&gt;.&lt;BR&gt;All Rights Reserved.&lt;BR&gt;&lt;BR&gt;Gmail Herald is made possible by the &lt;a href=&quot;http://sourceforge.net/projects/gherald&quot;&gt;GHerald&lt;/a&gt; open source project.</source>
         <oldsource>Gmail Herald v %1&lt;BR&gt;Copyright © 2010-2013 &lt;a href=&quot;mailto:vahancho@gmail.com&quot;&gt;Vahan Aghajanyan&lt;/a&gt;.&lt;BR&gt;All Rights Reserved.&lt;BR&gt;&lt;BR&gt;Gmail Herald is made possible by the &lt;a href=&quot;http://sourceforge.net/projects/gherald&quot;&gt;GHerald&lt;/a&gt; open source project.</oldsource>
         <translation>Gmail Herald %1&lt;BR&gt;Copyright © 2010-2013 &lt;a href=&quot;mailto:vahancho@gmail.com&quot;&gt;Vahan Aghajanyan&lt;/a&gt;.&lt;BR&gt;Все права защищены.&lt;BR&gt;&lt;BR&gt;Gmail Herald создан благодаря проекту с открытым кодом &lt;a href=&quot;http://sourceforge.net/projects/gherald&quot;&gt;GHerald&lt;/a&gt;.</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="54"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="55"/>
         <source>&lt;BR&gt;&lt;BR&gt;&lt;img src=&quot;:icons/warning&quot; width=&quot;16&quot; height=&quot;16&quot;&gt;&lt;small&gt; Gmail Herald %1 is available. You can download new version from the project&apos;s &lt;a href=&quot;http://sourceforge.net/projects/gherald/files&quot;&gt;web site&lt;/a&gt;.&lt;/small&gt;</source>
         <oldsource>&lt;BR&gt;&lt;BR&gt;&lt;img src=&quot;:icons/warning&quot; width=&quot;16&quot; height=&quot;16&quot;&gt;&lt;small&gt; Gmail Herald v %1 is now available! You can download new version from the project &lt;a href=&quot;http://sourceforge.net/projects/gherald/files&quot;&gt;web site&lt;/a&gt;.&lt;/small&gt;</oldsource>
         <translation>&lt;BR&gt;&lt;BR&gt;&lt;img src=&quot;:icons/warning&quot; width=&quot;16&quot; height=&quot;16&quot;&gt;&lt;small&gt; Доступна новая версия %1 Gmail Herald. Вы можете скачать новую версию на &lt;a href=&quot;http://sourceforge.net/projects/gherald/files&quot;&gt;сайте проекта&lt;/a&gt;.&lt;/small&gt;</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="58"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="59"/>
         <source>Gmail Herald %1 is available.
 You can download new version from the project&apos;s web site.</source>
         <translation>Доступна новая версия Gmail Herald %1.
 Вы можете скачать новую версию с веб сайта проекта.</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="62"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="63"/>
         <source>Gmail Herald Update</source>
         <translation>Обновление Gmail Herald</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="63"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="64"/>
         <source>Do you want to download it now?</source>
         <translation>Желаете ли скачать ее сейчас?</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="64"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="65"/>
         <source>Gmail Herald is up to date.</source>
         <translation>Это последняя версия Gmail Herald.</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="65"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="66"/>
         <source>Check for Updates</source>
         <translation>Проверить Обновления</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="72"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="73"/>
         <source>View &amp;Inbox</source>
         <translation>&amp;Открыть ящик</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="73"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="74"/>
         <source>&amp;Check Mail Now</source>
         <translation>&amp;Проверить почту</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="74"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="75"/>
         <source>Tell me A&amp;gain...</source>
         <translation>&amp;Сообщить повторно...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="75"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="76"/>
         <source>Change &amp;User/Password...</source>
         <oldsource>Change &amp;User...</oldsource>
         <translation>Сменить &amp;логин/пароль...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="76"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="77"/>
         <source>&amp;Options...</source>
         <translation>&amp;Настройки...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="77"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="78"/>
         <source>&amp;About</source>
         <oldsource>&amp;About...</oldsource>
         <translation>О п&amp;рограмме</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="78"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="79"/>
         <source>E&amp;xit</source>
         <translation>&amp;Выход</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="81"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="82"/>
         <source>No unread mail</source>
         <translation>Нет непрочитанной почты</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="82"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="83"/>
         <source>%1 unread conversation</source>
         <translation>%1 непрочитанных сообщений</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="83"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="84"/>
         <source>&lt;p&gt;Your inbox contains no unread conversations.&lt;/p&gt;</source>
         <translation>&lt;p&gt;Ваш почтовый ящик не содержит непрочитанных сообщений.&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="102"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="97"/>
+        <source>Connect to Gmail</source>
+        <translation>Соединиться с Gmail</translation>
+    </message>
+    <message>
+        <location filename="gherald/strings/guiStrings.cpp" line="103"/>
         <source>&amp;Mail checking preferences</source>
         <translation>&amp;Настройки проверки почты</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="108"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="109"/>
         <source>&amp;User interface language</source>
         <oldsource>User interface language</oldsource>
         <translation>&amp;Язык интерфейса</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="111"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="112"/>
         <source>Hide window</source>
         <translation>Скрыть окно</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="93"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="94"/>
         <source>User name:</source>
         <translation>Пользователь:</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="94"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="95"/>
         <source>Password:</source>
         <translation>Пароль:</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="95"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="96"/>
         <source>&amp;Remember my password</source>
         <translation>&amp;Запомнить пароль</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="112"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="113"/>
+        <source>Mark as read</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gherald/strings/guiStrings.cpp" line="114"/>
         <source>Next message</source>
         <translation>Следующее сообщение</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="113"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="115"/>
         <source>Previous message</source>
         <translation>Предыдущее сообщение</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="114"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="116"/>
         <source>%1 of %2</source>
         <translation>%1 из %2</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="115"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="117"/>
         <source>OK</source>
         <translation>ОК</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="116"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="118"/>
         <source>Cancel</source>
         <translation>Отмена</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="96"/>
         <source>Connect to mail.google.com</source>
-        <translation>Соедениться с  mail.google.com</translation>
+        <translation type="obsolete">Соедениться с  mail.google.com</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="68"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="69"/>
         <source>English</source>
         <translation>Английский</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="69"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="70"/>
         <source>Russian</source>
         <translation>Русский</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="86"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="87"/>
         <source>A host name lookup is in progress...</source>
         <translation>Поиск сервера...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="87"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="88"/>
         <source>Connecting to the host...</source>
         <translation>Соединение с сервером...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="88"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="89"/>
         <source>Sending request to the server...</source>
         <translation>Отправлен запрос серверу...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="89"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="90"/>
         <source>Reading the server&apos;s response...</source>
         <translation>Чтение ответа от сервера...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="90"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="91"/>
         <source>Connected</source>
         <oldsource>Connected.</oldsource>
         <translation>Соединен</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="99"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="100"/>
         <source>Play sound on new mail</source>
         <oldsource>&amp;Play sound on new mail</oldsource>
         <translation>Воспроизвести звук при новом сообщении</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="100"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="101"/>
         <source>&amp;Run on system start up</source>
         <translation>&amp;Запускать при старте системы</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="101"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="102"/>
         <source>Check mail every:</source>
         <translation>Проверять почту каждые:</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="103"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="104"/>
         <source>minutes</source>
         <translation>минуты</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="104"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="105"/>
         <source>...</source>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="105"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="106"/>
         <source>Gmail Herald Options</source>
         <translation>Настройки Gmail Herald</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="106"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="107"/>
         <source>Browse for notification sound file</source>
         <translation>Поиск звукового файла</translation>
     </message>
     <message>
-        <location filename="strings/guiStrings.cpp" line="107"/>
+        <location filename="gherald/strings/guiStrings.cpp" line="108"/>
         <source>Sound (*.wav)</source>
         <translation>Файл звука (*.wav)</translation>
+    </message>
+</context>
+<context>
+    <name>Gmail</name>
+    <message>
+        <location filename="gmaillib/gmail.cpp" line="67"/>
+        <source>User credentials incomplete. Please provide user name and password.</source>
+        <translation>Данные пользователя не полные. Пожалуйста впишите имя пользователя и пароль.</translation>
+    </message>
+    <message>
+        <location filename="gmaillib/gmail.cpp" line="223"/>
+        <source>Cannot connect to the server.</source>
+        <translation>Невозможно соединиться с сервером.</translation>
+    </message>
+    <message>
+        <location filename="gmaillib/gmail.cpp" line="237"/>
+        <source>Failed to write into the socket.</source>
+        <translation>Failed to write into the socket.</translation>
     </message>
 </context>
 </TS>
